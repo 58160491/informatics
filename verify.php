75fbd4c2ea1fa42a586cb4d2932e6a4509e2b777
Palapol Hinsui <36034838+58160491@users.noreply.github.com>
2019-01-24 15:52:33 +0700

    Update verify.php

--- verify.php
+++ verify.php
@@ -1,5 +1,6 @@
 <?php
-$access_token = 'IGP1cCGoyG8ki9iUSVs2zFOMjBrgMzXa2ba278mL7jyeDku4U9zCcElEcil+p9q13Crzu6dKzm2f3qvVOQ499g19bNuGVFXmwBTjSqCkFYt/b134CwWlmqtlx2VLmlk4lYWtXRZ8/xWj4oWg9R5I5wdB04t89/1O/w1cDnyilFU=';
+$access_token = 'UUR/MwiKl31bGnLm+e29ji35yxI5JDANgLMe/I4HDQD4XXk3hp9tYPpG0S3jp4wXPmLMxm2T15u5uKF9hPZoBI3bmIsgBpmYQJsF4Z53rmjhUDOof2YjCIHREOn5qsnTBrp226WLz/DLVp0nFOZkkAdB04t89/1O/w1cDnyilFU=
+';
 
 $url = 'https://api.line.me/v1/oauth/verify';
 
